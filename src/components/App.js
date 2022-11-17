b11d72c042872e67316305e41b39d71e67d25319
mkgupta2000 <105657874+mkgupta2000@users.noreply.github.com>
2022-11-18 02:13:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
+  const [display1, setDisplay1] = useState(true);
+  const [display2, setDisplay2] = useState(false);
+  const displayHandler = () => {
+    if (display1 === true) {
+      setDisplay1(false);
+      setDisplay2(true);
+    } else {
+      setDisplay1(true);
+      setDisplay2(false);
+    }
+  };
   return (
     <div id="main">
-      <div id="welcome-div">
-        <h1>Welcome to Newton School!!</h1>
-      </div>
-      <div id="about-div">
-        <h1>
-          Newton School is a neo-university providing highly immersive tech
-          learning to millions of students, to tap into new-age tech
-          opportunities.
-        </h1>
-      </div>
-      <button id="toggle">Toggle</button>
+      {display1 && (
+        <div id="welcome-div">
+          <h1>Welcome to Newton School!!</h1>
+        </div>
+      )}
+      {display2 && (
+        <div id="about-div">
+          <h1>
+            Newton School is a neo-university providing highly immersive tech
+            learning to millions of students, to tap into new-age tech
+            opportunities.
+          </h1>
+        </div>
+      )}
+      <button id="toggle" onClick={displayHandler}>
+        Toggle
+      </button>
     </div>
   );
 };
